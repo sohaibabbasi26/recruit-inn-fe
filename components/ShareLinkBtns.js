7eb926fc5a446da2e227b1ec2d6fff8e65a2c037
AqibQasim <muhammad44aqib@gmail.com>
2024-04-15 19:28:16 +0500

    fixes

--- components/ShareLinkBtns.js
+++ components/ShareLinkBtns.js
@@ -7,19 +7,14 @@ const ShareLinkBtns = ({ onContinue , onBack , onClose, setCompletedStages, comp
 
     async function handleSendInvite() {
         await handleEmailInvite();
-        setMessage('An invitation has been sent to the candidate via email')
-        showSuccess();
-        onClose();
     }
-
     return (
         <>
             <div className={styles.btnsContainer} >
                 <button id={styles.backBtn} onClick={onClose} >Skip Invitation</button>
                 <button id={styles.forwardBtn} onClick={handleSendInvite} >Send Invite<Image src='/send.svg' width={navigationIconSize} height={navigationIconSize}  /></button>
             </div>
-        </> 
+        </>
     )
 }
-
 export default ShareLinkBtns;
