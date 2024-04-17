c2658977a3d3d4b2dbaddd2d2401bb96bfe3d722
AqibQasim <muhammad44aqib@gmail.com>
2024-04-17 20:38:37 +0500

    Fixes

---
+++ src/pages/client-signup.js
@@ -0,0 +1,15 @@
+
+import { useRouter } from 'next/router';
+import styles from './Login.module.css';
+import { useEffect, useState } from 'react';
+import LoginOverlay from '../../components/LoginOverlay';
+
+const Signup = () => {
+
+    return (
+        <>  
+            <h1>This is Client Signup</h1>
+        </>
+    )
+}
+export default Signup;
