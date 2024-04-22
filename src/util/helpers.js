a9bda6202c0a515ac64d4f4f97f506818878a004
Eloquence98 <ibneenayat1998@gmail.com>
2024-04-23 01:26:59 +0500

    fix helpers file

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,20 +1,20 @@
-export function getSvg(skill) {
-  const skillsList = {
-    css: "css",
-    sass: "sass",
-    javascript: "javascript",
-    js: "javascript",
-    react: "react",
-    reactjs: "react",
-    next: "nextjs",
-    nextjs: "nextjs",
-    node: "node",
-    python: "python",
-    mongodb: "mongodb",
-    mongo: "mongodb",
-    aws: "aws",
-  };
+const skillsList = {
+  css: "css",
+  sass: "sass",
+  javascript: "javascript",
+  js: "javascript",
+  react: "react",
+  reactjs: "react",
+  next: "nextjs",
+  nextjs: "nextjs",
+  node: "node",
+  python: "python",
+  mongodb: "mongodb",
+  mongo: "mongodb",
+  aws: "aws",
+};
 
+export function getSvg(skill) {
   const sk = skill.replace(" ", "").toLowerCase();
 
   if (skillsList.hasOwnProperty(sk)) return `/${skillsList[sk]}.svg`;
