276024d92e40d0cad7f75fa8c00c637020de021e
sohaib abbasi <syedzubairalam123@gmail.com>
2024-04-23 18:04:08 +0500

    fixes v2.0.9

--- components/TopContainer.js
+++ components/TopContainer.js
@@ -31,55 +31,36 @@ const TopContainer = ({setDescription,description}) => {
         console.log('value:', value);
         console.log('description:', description)
     }, [value]);
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 0dbff317daf4bc2a35a0677d254f4fdcbaf03c7d
-    useEffect(() => {
-        const handleKeyDown = (event) => {
-          if (false) {
-            event.preventDefault();
-<<<<<<< HEAD
+    // useEffect(() => {
+    //     const handleKeyDown = (event) => {
+    //       if (false) {
+    //         event.preventDefault();
     
-            const activeElement = document.activeElement;
+    //         const activeElement = document.activeElement;
     
-=======
-            const activeElement = document.activeElement;
->>>>>>> 0dbff317daf4bc2a35a0677d254f4fdcbaf03c7d
-           if (activeElement.tagName === 'INPUT' && activeElement.form) {
-              const form = activeElement.form;
-              const submitButton = form.querySelector('[type="submit"]');
-              if (submitButton) {
-                submitButton.click();
-              }
-            } else {
-<<<<<<< HEAD
-              const continueButton = document.getElementById('RightBottomBtns_forwardBtn__83dJ2'); 
-=======
-              const continueButton = document.getElementById('RightBottomBtns_forwardBtn__83dJ2');
->>>>>>> 0dbff317daf4bc2a35a0677d254f4fdcbaf03c7d
-              if (continueButton) {
-                continueButton.click();
-              }
-            }
-          }
-        };
-<<<<<<< HEAD
+    //        if (activeElement.tagName === 'INPUT' && activeElement.form) {
+    //           const form = activeElement.form;
+    //           const submitButton = form.querySelector('[type="submit"]');
+    //           if (submitButton) {
+    //             submitButton.click();
+    //           }
+    //         } else {
+    //           const continueButton = document.getElementById('RightBottomBtns_forwardBtn__83dJ2'); 
+    //           if (continueButton) {
+    //             continueButton.click();
+    //           }
+    //         }
+    //       }
+    //     };
     
-        window.addEventListener('keydown', handleKeyDown);
+    //     window.addEventListener('keydown', handleKeyDown);
     
-=======
-        window.addEventListener('keydown', handleKeyDown);
->>>>>>> 0dbff317daf4bc2a35a0677d254f4fdcbaf03c7d
-        return () => {
-          window.removeEventListener('keydown', handleKeyDown);
-        };
-      }, []);
-<<<<<<< HEAD
-
-=======
->>>>>>> 0dbff317daf4bc2a35a0677d254f4fdcbaf03c7d
+    //     return () => {
+    //       window.removeEventListener('keydown', handleKeyDown);
+    //     };
+    //   }, []);
     // useEffect(() => {
     //     if (description !== undefined && value !== description) {
     //         setValue(description);
