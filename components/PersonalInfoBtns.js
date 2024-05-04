6c20dd039933b24debd64c23dfab5c4c1af1585c
nameer6 <nameeruddin6@gmail.com>
2024-05-04 17:43:58 +0500

    fixes from mac forget pass screen

--- components/PersonalInfoBtns.js
+++ components/PersonalInfoBtns.js
@@ -1,19 +1,22 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 import styles from './RightBottomBtns.module.css';
 
-import Image from 'next/image';
-
 const PersonalInfoBtns = ({ showSuccess, validateEmailReceiver, fillValidity, showError, showErrorMessage ,setMessage, onContinue , onBack , setCompletedStages, completedStages, onClose }) => {
+    const router = useRouter();
 
     const navigationIconSize = 30;
 
     const handleContinue = () => {
         onContinue();
     }
-
+    const handleback = () => {
+        router.push('/');
+    }
     return (
         <>
             <div className={styles.btnsContainer} >
-                {/* <button id={styles.backBtn} onClick={onBack}><Image src='/backward.svg' width={navigationIconSize} height={navigationIconSize} />Back</button> */}
+                <button id={styles.backBtn} onClick={handleback}><Image src='/backward.svg' width={navigationIconSize} height={navigationIconSize} />Back</button>
                 <button id={styles.forwardBtn} onClick={handleContinue} >Continue <Image src='/Forward.svg' width={navigationIconSize} height={navigationIconSize}  /></button>
             </div>
         </> 
