342fd19554f1e565b477ce7452708fe7d88a293d
Eloquence98 <ibneenayat1998@gmail.com>
2024-05-06 17:28:40 +0500

    try tempContainer on empty reports

--- components/ClientSignupOverlay.js
+++ components/ClientSignupOverlay.js
@@ -497,7 +497,7 @@ const ClientSignUpOverlay = ({
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${adminToken}`,
+            // Authorization: `Bearer ${adminToken}`,
           },
           body: JSON.stringify(requestBody),
         }
