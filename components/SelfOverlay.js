6f4d7aea6aa90db426bcfb17dfe004cebee6eb51
Eloquence98 <ibneenayat1998@gmail.com>
2024-05-09 20:27:54 +0500

    Revert "fixes"
    This reverts commit c27744bd838d18345ca2fee514e48ba801b6ba03.

--- components/SelfOverlay.js
+++ components/SelfOverlay.js
@@ -136,27 +136,31 @@ const SelfOverlay = ({
     ) {
       setMessage("Please fill all the fields first");
       showError();
+
+      if (currentStage === stages.PERSONAL_INFO && emailAlreadyExists) {
+        setMessage("Email is already registered.");
+        showError();
+        return;
+      } else {
+        setCurrentStage(stages.VERIFICATION);
+      }
+      
       return;
-    } else if (
+    }
+    else if (
       currentStage === stages.PERSONAL_INFO &&
       !validateEmailReceiver()
     ) {
       setMessage("Entered email is not valid");
       showError();
       return;
-    } else if (
-        currentStage === stages.PERSONAL_INFO &&
-      !validateContactReciever()
-    ){
-        setMessage("Entered contact is not valid");
-      showError();
-      return;
-    }
+    } 
     else if (currentStage === stages.PERSONAL_INFO && password.length < 8) {
       setMessage("Password must be at least 8 characters long ");
       showError();
       return;
-    } else if (
+    }
+    else if (
       currentStage === stages.PERSONAL_INFO &&
       !validateNumber(contact)
     ) {
