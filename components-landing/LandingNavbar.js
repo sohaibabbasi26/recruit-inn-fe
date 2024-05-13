9141696616b1afcd63818da6e5c2f1d472ec3bab
Eloquence98 <ibneenayat1998@gmail.com>
2024-05-14 00:44:09 +0500

    dark mode button

--- components-landing/LandingNavbar.js
+++ components-landing/LandingNavbar.js
@@ -5,109 +5,193 @@ import Image from "next/image";
 import { useState } from "react";
 import styles from "./LandingNavbar.module.css";
 import style from "./styles.module.css";
+import { useTheme } from "next-themes";
 
+const LandingNavbar = ({
+  scrollToRef,
+  howItWorksRef,
+  LandingThirdRef,
+  HeroRef,
+}) => {
+  const [menu, setMenu] = useState(false);
+  const { theme, setTheme } = useTheme();
 
-const LandingNavbar = ({scrollToRef, howItWorksRef,LandingThirdRef , HeroRef}) => {
-    const [menu, setMenu] = useState(false);
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
-    return (
-        <>
-            {/* leptup sacreen */}
-            <>
-            <div className=" max-sm:hidden bg-black" >
-            <div className='h-[10vh] w-full flex bg-black justify-center max-lg:hidden z-50 fixed' >
-                <div className="w-90p h-full flex justify-between items-center text-white">
-                    <div className="flex w-50p gap-20 items-center">
-                        <h2 onClick={() => scrollToRef(HeroRef)} className="max-lg:text-sm max-xl:text-xl  text-3xl font-bold font-poppins flex gap-3 cursor-pointer">
+  return (
+    <>
+      {/* leptup sacreen */}
+      <>
+        <div className=" max-sm:hidden bg-black">
+          <div className="h-[10vh] w-full flex bg-black justify-center max-lg:hidden z-50 fixed">
+            <div className="w-90p h-full flex justify-between items-center text-white">
+              <div className="flex w-50p gap-20 items-center">
+                <h2
+                  onClick={() => scrollToRef(HeroRef)}
+                  className="max-lg:text-sm max-xl:text-xl  text-3xl font-bold font-poppins flex gap-3 cursor-pointer"
+                >
+                  <Image src="/logo (3).png" height={30} width={30} />
+                  recruitinn.ai
+                </h2>
+                <button
+                  className="px-6 py-4 text-black dark:text-white bg-gray-100 dark:bg-gray-900 rounded-3xl text-sm md:text-base lg:text-xl"
+                  onClick={toggleTheme}
+                >
+                  Dark
+                </button>
 
-                            <Image src="/logo (3).png" height={30} width={30} />
-                            recruitinn.ai
-                        </h2>
-
-                        <ul className="flex gap-10 max-xl:gap-3 max-xl:w-[70%]">
-                            <li onClick={() => scrollToRef(howItWorksRef)} className="text-md font-semibold max-xl:text-sm cursor-pointer ">How It Works</li>
-                            <li onClick={() => scrollToRef(LandingThirdRef)} className="text-md font-semibold max-xl:text-sm cursor-pointer ">About Us</li>
-                        </ul>
-                    </div>
-                    <div className="w-60p items-center flex justify-end gap-4 max-xl:gap-2">
-                            <span className="text-md max-xl:text-sm font-semibold cursor-pointer"><a href="https://app.recruitinn.ai/candidate-self"> Apply As A Candidate</a></span>
-                            <div className="flex gap-8 max-xl:gap-2">
-                            <div className={`${styles.dropdown} px-10 py-3.5 text-mnmd max-xl:text-sm bg-darkPurple rounded-3xl fnt-semibold`}>Login
-                            <ul className={styles.dropdown_menu}>
-                                <li><a href="https://app.recruitinn.ai/candidate-login">Login As Candidate</a></li>
-                                <li><a href="https://app.recruitinn.ai/client-login">Login As Client</a></li>
-                            </ul>
-                            </div>
-                            <a href="https://app.recruitinn.ai/client-signup">
-  <button
-    className="px-10 py-3.5 text-md max-xl:text-sm btn-gradient rounded-3xl font-semibold"
-    style={{
-      transition: "background-color 0.3s, color 0.3s", // Add transition for smooth hover effect
-      backgroundColor: "#6137db", // Default background color hello
-      color: "#fff", // Default text color
-      border: "none", // Remove default button border
-    }}
-    onMouseEnter={(e) => {
-      e.target.style.backgroundColor = "#D3CFFC"; // Change background color on hover
-      e.target.style.color = "rgb(0 1 0 / var(--tw-text-opacity))"; // Change text color on hover
-    }}
-    onMouseLeave={(e) => {
-      e.target.style.backgroundColor = ""; // Restore default background color
-      e.target.style.color = ""; // Restore default text color
-    
-    }}
-  >
-    Recruit A Talent
-  </button>
-</a>                            </div>
-                    </div>
+                <ul className="flex gap-10 max-xl:gap-3 max-xl:w-[70%]">
+                  <li
+                    onClick={() => scrollToRef(howItWorksRef)}
+                    className="text-md font-semibold max-xl:text-sm cursor-pointer "
+                  >
+                    How It Works
+                  </li>
+                  <li
+                    onClick={() => scrollToRef(LandingThirdRef)}
+                    className="text-md font-semibold max-xl:text-sm cursor-pointer "
+                  >
+                    About Us
+                  </li>
+                </ul>
+              </div>
+              <div className="w-60p items-center flex justify-end gap-4 max-xl:gap-2">
+                <span className="text-md max-xl:text-sm font-semibold cursor-pointer">
+                  <a href="https://app.recruitinn.ai/candidate-self">
+                    {" "}
+                    Apply As A Candidate
+                  </a>
+                </span>
+                <div className="flex gap-8 max-xl:gap-2">
+                  <div
+                    className={`${styles.dropdown} px-10 py-3.5 text-mnmd max-xl:text-sm bg-darkPurple rounded-3xl fnt-semibold`}
+                  >
+                    Login
+                    <ul className={styles.dropdown_menu}>
+                      <li>
+                        <a href="https://app.recruitinn.ai/candidate-login">
+                          Login As Candidate
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://app.recruitinn.ai/client-login">
+                          Login As Client
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <a href="https://app.recruitinn.ai/client-signup">
+                    <button
+                      className="px-10 py-3.5 text-md max-xl:text-sm btn-gradient rounded-3xl font-semibold"
+                      style={{
+                        transition: "background-color 0.3s, color 0.3s", // Add transition for smooth hover effect
+                        backgroundColor: "#6137db", // Default background color hello
+                        color: "#fff", // Default text color
+                        border: "none", // Remove default button border
+                      }}
+                      onMouseEnter={(e) => {
+                        e.target.style.backgroundColor = "#D3CFFC"; // Change background color on hover
+                        e.target.style.color =
+                          "rgb(0 1 0 / var(--tw-text-opacity))"; // Change text color on hover
+                      }}
+                      onMouseLeave={(e) => {
+                        e.target.style.backgroundColor = ""; // Restore default background color
+                        e.target.style.color = ""; // Restore default text color
+                      }}
+                    >
+                      Recruit A Talent
+                    </button>
+                  </a>{" "}
                 </div>
+              </div>
             </div>
+          </div>
+        </div>
+      </>
+      {/* mobile de screen buthur */}
+      <div className="h-[10vh] bg-black">
+        <div className="h-[10vh] bg-black z-50 w-full flex max-lg:items-center max-lg:justify-center hidden max-lg:flex fixed ">
+          <div className="w-100p h-[2rem] flex flex-col max-sm:justify-between max-sm:items-center text-white ">
+            <div className="flex w-[100%] justify-between items-center">
+              <div className="flex items-center gap-3 max-lg:ml-[1.5rem] max-sm:ml-[1rem]">
+                <Image src="/logoo.svg" height={30} width={30} alt="logo" />
+                <h2 className="text-2xl max-sm:text-xl font-bold font-poppins">
+                  recruitinn.ai
+                </h2>
+              </div>
+              <div
+                className="max-lg:flex hidden max-lg:mr-[1.5rem] max-sm:mr-[1rem]"
+                onClick={() => setMenu(!menu)}
+              >
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  ></path>
+                </svg>
+              </div>
             </div>
-        </>
-            {/* mobile de screen buthur */}
-            <div className="h-[10vh] bg-black" >
-            <div className='h-[10vh] bg-black z-50 w-full flex max-lg:items-center max-lg:justify-center hidden max-lg:flex fixed '>
-                <div className="w-100p h-[2rem] flex flex-col max-sm:justify-between max-sm:items-center text-white ">
-                    <div className="flex w-[100%] justify-between items-center">
-                        <div className="flex items-center gap-3 max-lg:ml-[1.5rem] max-sm:ml-[1rem]">
-                            <Image src="/logoo.svg" height={30} width={30} alt="logo"/>
-                            <h2 className="text-2xl max-sm:text-xl font-bold font-poppins">recruitinn.ai</h2>
-                        </div>
-                        <div className="max-lg:flex hidden max-lg:mr-[1.5rem] max-sm:mr-[1rem]" onClick={() => setMenu(!menu)}>
-                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
-                        </div>
-                    </div>  
 
-                    <div className={`flex items-center gap-8 ${menu ? "flex" : "hidden"} max-lg:hidden`}>
-                        <span className="text-sm font-semibold">Apply As An Engineer</span>
-                        <div className="flex gap-8">
-                            <button className="px-10 py-3.5 text-sm bg-darkPurple font-semibold sm:rounded-3xl">Login</button>
-                            <button className="px-10 py-3.5 text-sm btn-gradient font-semibold sm:rounded-3xl">Recruit A Talent</button>
-                        </div>
-                    </div>
+            <div
+              className={`flex items-center gap-8 ${
+                menu ? "flex" : "hidden"
+              } max-lg:hidden`}
+            >
+              <span className="text-sm font-semibold">
+                Apply As An Engineer
+              </span>
+              <div className="flex gap-8">
+                <button className="px-10 py-3.5 text-sm bg-darkPurple font-semibold sm:rounded-3xl">
+                  Login
+                </button>
+                <button className="px-10 py-3.5 text-sm btn-gradient font-semibold sm:rounded-3xl">
+                  Recruit A Talent
+                </button>
+              </div>
+            </div>
 
-                    {menu && (
-                        <div className="absolute top-[100%]  z-50  flex flex-col justify-center items-center gap-4 max-lg:flex w-full  left-0 bg-black py-4">
-                            <span className="text-sm font-semibold text-white max-lg:py-2 max-lg:border-b max-lg:border-darkPurple">Apply As An Engineer</span>
-                            <button className="px-10 py-3.5 max-lg:py-2 max-lg:border-b max-lg:border-darkPurple text-sm bg-transparent font-semibold max-lg:block hidden">Login</button>
-                            <button className="px-10 py-3.5 max-lg:py-2 text-sm bg-transparent max-lg:border-b max-lg:border-darkPurple font-semibold max-lg:block hidden">Recruit A Talent</button>
-                            <ul className="flex flex-col items-center gap-4">
-                                <li onClick={() => scrollToRef(howItWorksRef)} className="text-sm font-semibold    p-2 max-lg:border-b max-lg:border-darkPurple">How It Works</li>
-                                <li className="text-sm font-semibold p-2 max-lg:border-b max-lg:border-darkPurple">About Us</li>
-                            </ul>
-                        </div>  
-                    )}
-                </div>
-            </div>
-            </div>
-        </>
-    );
+            {menu && (
+              <div className="absolute top-[100%]  z-50  flex flex-col justify-center items-center gap-4 max-lg:flex w-full  left-0 bg-black py-4">
+                <span className="text-sm font-semibold text-white max-lg:py-2 max-lg:border-b max-lg:border-darkPurple">
+                  Apply As An Engineer
+                </span>
+                <button className="px-10 py-3.5 max-lg:py-2 max-lg:border-b max-lg:border-darkPurple text-sm bg-transparent font-semibold max-lg:block hidden">
+                  Login
+                </button>
+                <button className="px-10 py-3.5 max-lg:py-2 text-sm bg-transparent max-lg:border-b max-lg:border-darkPurple font-semibold max-lg:block hidden">
+                  Recruit A Talent
+                </button>
+                <ul className="flex flex-col items-center gap-4">
+                  <li
+                    onClick={() => scrollToRef(howItWorksRef)}
+                    className="text-sm font-semibold    p-2 max-lg:border-b max-lg:border-darkPurple"
+                  >
+                    How It Works
+                  </li>
+                  <li className="text-sm font-semibold p-2 max-lg:border-b max-lg:border-darkPurple">
+                    About Us
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LandingNavbar;
-
-
 
 // import { useState } from   'react';
 // import Image from "next/image";
@@ -137,7 +221,7 @@ export default LandingNavbar;
 //                             <li className="text-sm font-semibold">How It Works</li>
 //                             <li className="text-sm font-semibold">About Us</li>
 //                         </ul>
-                        
+
 //                         <div className="flex flex-col sm:flex-row gap-4 max-sm:gap-8">
 //                             <span className="text-sm font-semibold block max-sm:hidden">Apply As An Engineer</span>
 //                             <button className="px-4 max-sm:px-10 py-2 sm:py-3.5 text-xs max-sm:text-sm bg-darkPurple rounded-3xl font-semibold">Login</button>
@@ -176,7 +260,7 @@ export default LandingNavbar;
 //     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 nav">
 //       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 //         <Link
-//           to="Overview"  
+//           to="Overview"
 //           smooth={true}
 //           duration={800}
 //           className="flex items-center space-x-3 rtl:space-x-reverse cursor-pointer"
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,22 @@
-import '@/styles/globals.css';
+import "@/styles/globals.css";
 // import '../../components-landing/styles.css'; // Uncomment if needed
-import { useEffect } from 'react';
-import { ActiveItemProvider } from '@/contexts/ActiveItemContext';
-import { TestProvider } from '@/contexts/QuestionsContent';
-import { ExpertiseItemProvider } from '@/contexts/ExpertiseContext';
-import NameProvider from '@/contexts/NameProvider';
-import { ActiveFlowProvider } from '@/contexts/ActiveFlowContext';
-import { TestStateProvider } from '@/contexts/TestRequirementContext';
+import { useEffect } from "react";
+import { ActiveItemProvider } from "@/contexts/ActiveItemContext";
+import { TestProvider } from "@/contexts/QuestionsContent";
+import { ExpertiseItemProvider } from "@/contexts/ExpertiseContext";
+import NameProvider from "@/contexts/NameProvider";
+import { ActiveFlowProvider } from "@/contexts/ActiveFlowContext";
+import { TestStateProvider } from "@/contexts/TestRequirementContext";
+import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
-
   return (
     <TestStateProvider>
       <ActiveFlowProvider>
         <ActiveItemProvider>
-          {}
-          <Component {...pageProps} />
+          <ThemeProvider attribute="class" defaultTheme="light">
+            {}
+            <Component {...pageProps} />
+          </ThemeProvider>
         </ActiveItemProvider>
       </ActiveFlowProvider>
     </TestStateProvider>
