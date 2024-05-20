7ca652687c3eb296752ec2fb0ce78becd659cf09
Eloquence98 <ibneenayat1998@gmail.com>
2024-05-20 17:09:07 +0500

    add test language (English)

--- components/TestInstruction.js
+++ components/TestInstruction.js
@@ -1,42 +1,39 @@
-import styles from './TestInstruction.module.css';
-import Image from 'next/image';
+import styles from "./TestInstruction.module.css";
+import Image from "next/image";
 
-const TestInstruction = ({onClose}) => {
+const TestInstruction = ({ onClose }) => {
+  return (
+    <>
+      <div className={styles.backDropContainer}>
+        <div className={styles.instructionsBox}>
+          <div className={styles.container}>
+            <h3>Assesment Instructions</h3>
 
-    return (
-        <>
-            <div className={styles.backDropContainer}>
-                <div className={styles.instructionsBox}>
-                    <div className={styles.container}>
-                        <h3>Assesment Instructions</h3>
-
-                        {/* instruction lists */}
-                        <div className={styles.instructionsContainer}>
-
-                            <ul className={styles['custom-bullets']}>
-                                <li>
-                                    Make sure your connection is stable.
-                                </li>
-                                <li>
-                                Your score will reflect on your profile.                                </li>
-                                <li>
-                                Avoid Refreshing your page during interview                               </li>
-                                <li>
-                                Make sure there’s no background noise while answering the questions.                                </li>
-                            </ul>
-                        </div>
-
-                        {/* closing button */}
-                        <div className={styles.lowerContainer}>
-                            <button onClick={onClose}>Let's Start <Image src='/Forward.svg' width={15} height={15} /> </button>
-                        </div>
-
-                    </div>
-                </div>
+            {/* instruction lists */}
+            <div className={styles.instructionsContainer}>
+              <ul className={styles["custom-bullets"]}>
+                <li>Make sure your connection is stable.</li>
+                <li>Your score will reflect on your profile. </li>
+                <li>Avoid Refreshing your page during interview </li>
+                <li>Give your answers in English </li>
+                <li>
+                  Make sure there’s no background noise while answering the
+                  questions.{" "}
+                </li>
+              </ul>
             </div>
-        </>
-    )
-}
+
+            {/* closing button */}
+            <div className={styles.lowerContainer}>
+              <button onClick={onClose}>
+                Let's Start <Image src="/Forward.svg" width={15} height={15} />{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default TestInstruction;
-
