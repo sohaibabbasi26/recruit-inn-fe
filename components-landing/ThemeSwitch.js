65e0b6a4203bc4b4a84f03e9ca34bd07187b8084
Eloquence98 <ibneenayat1998@gmail.com>
2024-06-03 11:39:05 +0500

    adjust theme switch button

--- components-landing/ThemeSwitch.js
+++ components-landing/ThemeSwitch.js
@@ -18,12 +18,13 @@ function ThemeSwitch() {
 
   return (
     <>
-      <div className="mr-0 ml-4 bg-white-purple-shade dark:bg-[#141414] border border-light-grey dark:border-transparent text-steel rounded-[3rem] px-3 py-[0.45rem]  cursor-pointer grid grid-cols-2 grid-rows-1 max-w-[77px] relative -top-0 max-lg:-top-2 max-lg:mr-4 max-lg:ml-auto max-lg:mt-3 max-lg:py-[2px] max-lg:px-2">
+      <div className="mr-0 ml-4 bg-white-purple-shade dark:bg-[#141414] border border-light-grey dark:border-transparent text-steel rounded-[3rem] px-3 py-[0.45rem]  cursor-pointer grid grid-cols-2 grid-rows-1 max-w-[82px] max-lg:max-w-[77px] relative -top-0 max-lg:-top-2 max-lg:mr-4 max-lg:ml-auto max-lg:mt-3 max-lg:py-[2px] max-lg:px-2">
         <Image
           src="/moon.svg"
           width={33}
           height={33}
           onClick={() => setTheme("dark")}
+          className="relative -left-1"
         />
         <Image
           // src="/sun.svg"
@@ -31,6 +32,7 @@ function ThemeSwitch() {
           width={33}
           height={33}
           onClick={() => setTheme("light")}
+          className="relative -right-1"
         />
         <div
           className={`${styles.ball} ${
