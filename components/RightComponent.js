7a487e88c79eb84060c587bf621ec0d5ca4eb2b4
Eloquence98 <ibneenayat1998@gmail.com>
2025-01-08 15:19:35 +0500

    fix: resolve client dashboard's RightComponent grid styles

--- components/RightComponent.js
+++ components/RightComponent.js
@@ -6,6 +6,7 @@ import { Grenze } from "next/font/google";
 
 const Graph = dynamic(() => import("../components/Graph"), { ssr: false });
 const RightComponent = ({
+  activeItem,
   preprocessedCandidates,
   setShowOverlay,
   interviewCount,
@@ -15,27 +16,26 @@ const RightComponent = ({
   const openAddJobHandler = () => {
     setShowOverlay(true);
   };
+  const onDashboard = activeItem === "Dashboard" ? "dashboard" : "";
 
-  function getInterviewCountColor(interviewCount){
-    if(interviewCount <= 5){
-        return 'red';
-    }
-    else if(interviewCount >= 6 && interviewCount < 15){
-        return "#FAD02C";
-    }
-    else{
-      return 'green';
+  function getInterviewCountColor(interviewCount) {
+    if (interviewCount <= 5) {
+      return "red";
+    } else if (interviewCount >= 6 && interviewCount < 15) {
+      return "#FAD02C";
+    } else {
+      return "green";
     }
   }
 
   return (
     <>
       <div className={` ${styles.superContainer}`}>
-        <div className={styles.masterContainer}>
+        <div className={`${styles.masterContainer} ${styles[onDashboard]}`}>
           <div className={styles.btnsDiv}>
             <button className={styles.addJobBtn} onClick={openAddJobHandler}>
               <Image src="/Add.svg" width={iconSize} height={iconSize} /> Create
-            a Job
+              a Job
             </button>
             {/* <button className={styles.notificationsBtn}>
               <Image
@@ -46,8 +46,11 @@ const RightComponent = ({
             </button> */}
           </div>
           <div>
-            <div className={styles.countContainer} >
-              Interviews Remaining: <span style={{color: getInterviewCountColor(interviewCount)}}>{interviewCount}</span>
+            <div className={styles.countContainer}>
+              Interviews Remaining:{" "}
+              <span style={{ color: getInterviewCountColor(interviewCount) }}>
+                {interviewCount}
+              </span>
             </div>
           </div>
           <Graph preprocessedCandidates={preprocessedCandidates} />
