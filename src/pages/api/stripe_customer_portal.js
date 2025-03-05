a43a784a7b92bbbbf860d08642d6b310bb4e6132
Sanjay Kumar <sanjay1402k@gmail.com>
2025-03-05 16:55:29 +0500

    routing fix

--- src/pages/api/stripe_customer_portal.js
+++ src/pages/api/stripe_customer_portal.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
     const session = await stripe.billingPortal.sessions.create({
       customer: customerId,
-      return_url: process.env.NEXT_PUBLIC_URL, // Change to your return URL
+      return_url: `${process.env.NEXT_PUBLIC_URL}/client/`, // Change to your return URL
     });
 
     res.status(200).json({ url: session.url });
